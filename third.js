56731a0daa2aed1c13b2398187622c0d042cf5cb
swetha <swethameghu1402@gmail.com>
2026-01-28 22:14:08 +0530

    final commit

---
+++ third.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Simple JavaScript</h2>
+
+<button onclick="showMessage()">Click Me</button>
+
+<script>
+function showMessage() {
+    alert("Hello! This is JavaScript");
+}
+</script>
+
+</body>
+</html>
